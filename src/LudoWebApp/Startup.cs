ce51e8c12cc9b8cc7b5ce0bffba09b4c8af23ecb
Phatchari <gataij@hotmail.com>
2019-01-31 15:41:06 +0100

    spelplanen syns

--- src/LudoWebApp/Startup.cs
+++ src/LudoWebApp/Startup.cs
@@ -53,7 +53,7 @@ namespace LudoWebApp
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Ludo}/{action=Index}/{id?}");
             });
         }
     }
