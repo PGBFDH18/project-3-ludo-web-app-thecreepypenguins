87d32222796528e41c83e0a65ef8c20889d86cf7
Lena <perlen93@hotmail.com>
2019-02-01 15:25:56 +0100

    Creconection to API
    A GET on ludo/{id}

---
+++ src/LudoWebApp/LudoModels/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LudoWebApp.LudoModels
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+        public string PlayerColor { get; set; }
+        //public Piece[] Pieces { get; set; }
+        public int Offset { get; set; }
+    }
+}
