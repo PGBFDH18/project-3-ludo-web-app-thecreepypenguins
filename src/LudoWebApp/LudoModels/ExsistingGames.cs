e889d5964616416a97664339b22e7e4b16f01b03
Viktor Persson <Viktor.persson91@gmail.com>
2019-02-04 17:50:00 +0100

    Created something and something

---
+++ src/LudoWebApp/LudoModels/ExsistingGames.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LudoWebApp.LudoModels
+{
+    public class ExsistingGames
+    {
+        public List<Player> _players = new List<Player>();
+       // private GameState _gameState = GameState.NotStarted;
+        private int currentPlayerId = 0;
+       // private IDiece _diece = null;
+
+
+    }
+}
