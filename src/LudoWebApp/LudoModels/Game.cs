e524ab96a7ad708e05e2b1a587dd73c0c93419b8
Phatchari <gataij@hotmail.com>
2019-02-07 21:26:37 +0100

    lagt till GameState ifrån API:et. GameState skrivs ut på view.

--- src/LudoWebApp/LudoModels/Game.cs
+++ src/LudoWebApp/LudoModels/Game.cs
@@ -7,9 +7,10 @@ namespace LudoWebApp.LudoModels
 {
     public class Game
     {
-        public int GameId { get; set;}
+        public int GameId { get; set; }
+        public GameState GameState { get; set; }
         public Player CurrentPlayer { get; set; }
         public List<Player> Players { get; set; }
-        public List<Piece> Pieces{ get; set; }
+        public List<Piece> Pieces { get; set; }
     }
 }
