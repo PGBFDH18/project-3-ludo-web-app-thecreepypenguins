624104db5012a4dd2886d7a2ecb315746914cb61
Lena <perlen93@hotmail.com>
2019-02-14 13:13:55 +0100

    Added some Validation in class player

--- src/LudoWebApp/LudoModels/Player.cs
+++ src/LudoWebApp/LudoModels/Player.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace LudoWebApp.LudoModels
 {
     public class Player
     {
+        [Range(1, 4)]
         public int PlayerId { get; set; }
         public string Name { get; set; }
+        [Range(1, 4)]
         public string PlayerColor { get; set; }
         public List<Piece> Pieces { get; set; }
         public int Offset { get; set; }
